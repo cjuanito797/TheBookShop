2c206f1648e50fe7654012e741f767658851ff54
Juan <jlizarraga@unomaha.edu>
2022-02-24 17:36:09 -0600

    Update admin.py

--- library/admin.py
+++ library/admin.py
@@ -1,21 +1,24 @@
 from .models import Book, Genre, Author
 from django.contrib import admin
 
+
 # Register your models here.
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
+@admin.register (Book)
+class BookAdmin (admin.ModelAdmin):
     list_display = ['title', 'slug', 'price', 'available', 'author']
     list_filter = ['available', ]
-    prepopulated_fields = {'slug':('title', )}
+    prepopulated_fields = {'slug': ('title',)}
 
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
+
+@admin.register (Author)
+class AuthorAdmin (admin.ModelAdmin):
     list_display = ['last_name', 'first_name', 'slug']
     list_filter = ['last_name']
-    prepopulated_fields = {'slug': ('last_name', )}
+    prepopulated_fields = {'slug': ('last_name',)}
 
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
+
+@admin.register (Genre)
+class GenreAdmin (admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name', )}
+    prepopulated_fields = {'slug': ('name',)}
