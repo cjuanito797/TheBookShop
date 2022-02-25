e2f36d42bce5e6a13e017c81de82ee25270397b8
Juan <jlizarraga@unomaha.edu>
2022-02-24 19:48:27 -0600

    Create admin.py

---
+++ orders/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+@admin.register(Order)
+class orderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user']
+
+@admin.register(OrderItem)
+class orderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', ]
